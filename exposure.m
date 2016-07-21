77f0cde2e72056bd22d4347cbe0418594a051f32
alboryck <alborcyk@utexas.edu>
2016-07-21 15:09:50 -0500

    exposure draft complete: begins debugging

--- exposure.m
+++ exposure.m
@@ -2,12 +2,11 @@ function exposure(Display, Joyconfig, Subject, path, arg)
 
 
 
-Config.blocks = 1;
 Config.imagecount = 20;
 Config.trials = 2*config.imagecount;
 Config.trialdur = 2;
 Config.rate_dur = 1;
-Config.date = sprintf('%s %2.0f:%02.0f',date,d(4),d(5));
+Config.date = sprintf('%s %2.0f:%02.0f',date, d(4), d(5));
 if fmri == 1
     Config.jitter = [4 5 6 8];
 else Config.jitter = 2;
@@ -59,34 +58,34 @@ pictype = [ones(n_images,1); zeros(n_images,1)];
 piclist = [randperm(n_images)'; randperm(n_images)'];
 trial_types = [pictype piclist];
 order = trial_types(randperm(size(trial_types,1)),:);
-data = struct.empty(Config.trials);
+Trial.data = struct.empty(Config.trials);
 jitter = BalanceTrials(Config.blocks*Config.trials,1,Config.jitter);
 
 for i = 1:Config.trials;
-    data(i).pictype = order(i,1);
+    Trial.data(i).pictype = order(i,1);
 
     if order(i,1) == 1
-    data(i).picname = Pics.high(order(i,2)).name;
+    Trial.data(i).picname = Pics.high(order(i,2)).name;
 	elseif order(i,1) == 0
         data(i).picname = Pics.low(order(i,2)).name;
     end
          
-	data(i).jitter = jitter(i);
-	data(i).fix_onset = NaN;
-	data(i).pic_onset = NaN;
-    data(i).rate_onset = NaN;
-    data(i).rate_RT = NaN;
-    data(i).rating = 5;
+	Trial.data(i).jitter = jitter(i);
+	Trial.data(i).fix_onset = NaN;
+	Trial.data(i).pic_onset = NaN;
+    Trial.data(i).rate_onset = NaN;
+    Trial.data(i).rate_RT = NaN;
+    Trial.data(i).rating = 5;
 end
 
 commandwindow;
 
-time = mri_sync(Display);
+
+Time.start = mri_sync(Display);
 
 DrawFormattedText(Display.window,'We are going to show you pictures of food. \n\n Press the joystick trigger to continue.','center','center',[255 255 255],50,[],[],1.5);
 Screen('Flip', Display.window);
 joystick_wait(Joyconfig);
-
 
 DrawFormattedText(w,'A green border will appear around the image, you will have one second to react with the joystick. \n\n Press the joystick trigger to continue.','center','center',COLORS.WHITE,50,[],[],1.5);
 Screen('Flip',w);
@@ -96,96 +95,84 @@ DrawFormattedText(w,'Pull the joystick toward you for foods that you do like. \n
 Screen('Flip',w);
 joystick_wait(Joyconfig);
 
-for block = 1:STIM.blocks
-    for trial = 1:STIM.trials
-        tcounter = (block-1)*STIM.trials + trial;
-        tpx = imread(getfield(SimpExp,'data',{tcounter},'picname'));
-        texture = Screen('MakeTexture',w,tpx);
+
+for i = 1:Config.trials
+    texture = Screen('MakeTexture', Display.window, imread(getfield(Trial,'data',{tcounter},'picname')));
+    Time.onset = draw_fixation_cross(Display, SimpExp.data(i).jitter);
+    SimpExp.data(i).fix_onset = Time.onset - Time.start;
+    
+    Screen('FillRect', w, [0 0 255], STIM.framerect + [-100 -100 100 100]);
+    Screen('DrawTexture',Display.window,texture,[],STIM.framerect);
+    Time.onset = Screen('Flip',w);
+    Trial.data(tcounter).pic_onset = Time.onset - Time.start;
+    WaitSecs(Config.trialdur - Config.rate_dur);
+    
+    Screen('FillRect', w, [0 255 0], STIM.framerect + [-100 -100 100 100]);
+    Screen('DrawTexture',w,texture,[],STIM.framerect);
+    Time.onset = Screen('Flip',w);
+    SimpExp.data(i).rate_onset = Time.onset - scan_sec;
         
-        % Fixation. 20160509cdt
-        DrawFormattedText(w,'+','center','center',COLORS.WHITE);
-        fixon = Screen('Flip',w);
-        SimpExp.data(tcounter).fix_onset = fixon - scan_sec;
-        WaitSecs(SimpExp.data(tcounter).jitter);
-        
-        % Intial display of food and instructions. 20160509cdt
-        Screen('DrawTexture',w,texture,[],STIM.framerect);
-%         DrawFormattedText(w,verbage,'center',(wRect(4)*.75),COLORS.WHITE);
-        if (usingKeyboard)
-            drawRatings([],w);
-        end
-        picon = Screen('Flip',w);
-        SimpExp.data(tcounter).pic_onset = picon - scan_sec;
-        WaitSecs(STIM.trialdur - STIM.rate_dur);
-        
-        % Time to rate the food. 20160509cdt
-        Screen('FillRect', w, COLORS.GREEN, STIM.framerect + [-BORDERSIZE -BORDERSIZE BORDERSIZE BORDERSIZE]);
-        Screen('DrawTexture',w,texture,[],STIM.framerect);
-%         DrawFormattedText(w,verbage,'center',(wRect(4)*.75),COLORS.GREEN);
-        if (usingKeyboard)
-            drawRatings([],w,1); %The 1 here turns everything green.
-        end
-        rateon = Screen('Flip',w);
-        SimpExp.data(tcounter).rate_onset = rateon - scan_sec;
-        
-        FlushEvents();
-        telap = 0;
-        while telap < STIM.rate_dur
-            telap = GetSecs() - rateon;
+    FlushEvents();
+    Time.response = 0;
+    while Time.response < Config.rate_dur
             
-            if (usingKeyboard)
-                [keyisdown, rt, keycode] = KbCheck();
-                if (keyisdown==1 && any(keycode(KEYS.all)))
-                    SimpExp.data(tcounter).rate_RT = rt - rateon;
+        Time.response = GetSecs() - Time.onset;
+            
+        Input = get_joystick_value(Joyconfig);
+        if Input.y > 5*Joyconfig.ymod
+            Screen('FillRect', Display.window, [0 255 0], STIM.bigrect + [-BORDERSIZE -BORDERSIZE BORDERSIZE BORDERSIZE]);
+            Screen('DrawTexture', Display.window, texture, [], STIM.framerectfar);
+            Screen('Flip',w);
+            SimpExp.data(i).rating = 9;
+            Config.rate_RT = GetSecs - Time.onset;
 
-                    rating = KbName(find(keycode));
-                    rating = str2double(rating(1));
+            Trial.data(i)
+            break
+        elseif Input.y < 5*Joyconfig.ymod
+            Screen('FillRect', Display.window, [0 255 0], STIM.smallrect + [-BORDERSIZE -BORDERSIZE BORDERSIZE BORDERSIZE]);
+            Screen('DrawTexture',Display.window, texture, [], STIM.smallrect);
+            Screen('Flip',Display.window);
+            SimpExp.data(i).rating = 1;
+            Config.rate_RT = GetSecs - Time.onset;
 
-                    Screen('DrawTexture',w,texture,[],STIM.framerect);
-                    drawRatings(keycode,w,1);
-%                     DrawFormattedText(w,verbage,'center',(wRect(4)*.75),COLORS.GREEN);
-                    Screen('Flip',w);
-                    WaitSecs(.25);
-                    if fmri == 1;
-                        rating = rating + 1;
-                    elseif fmri == 0 && rating == 0
-                        rating = 10;
-                    end
+            Trial.data(i)
+            break
 
-                    SimpExp.data(tcounter).rating = rating;
-                    break;
-                end
-            else % using joystick 20160509cdt
-                n = 0;
-                [x, y, z, buttons] = WinJoystickMex(n);
-                if (y < joystickCenter - joystickSensitivity)
-                    if isnan(SimpExp.data(tcounter).rate_RT)
-                        SimpExp.data(tcounter).rate_RT = GetSecs() - rateon;
-                    end
-                    Screen('FillRect', w, COLORS.GREEN, STIM.framerectfar + [-BORDERSIZE -BORDERSIZE BORDERSIZE BORDERSIZE]);
-                    Screen('DrawTexture',w,texture,[],STIM.framerectfar);
-%                     DrawFormattedText(w,verbage,'center',(wRect(4)*.75),COLORS.GREEN);
-                    Screen('Flip',w);
-                    SimpExp.data(tcounter).rating = 1;
-                    %WaitSecs(.25);
-                    fprintf('%d %d %d - %d %d %d %d \n', x, y, z, buttons(1), buttons(2), buttons(3), buttons(4));
-                elseif (y > joystickCenter + joystickSensitivity)
-                    if isnan(SimpExp.data(tcounter).rate_RT)
-                        SimpExp.data(tcounter).rate_RT = GetSecs() - rateon;
-                    end
-                    Screen('FillRect', w, COLORS.GREEN, STIM.framerectnear + [-BORDERSIZE -BORDERSIZE BORDERSIZE BORDERSIZE]);
-                    Screen('DrawTexture',w,texture,[],STIM.framerectnear);
-%                     DrawFormattedText(w,verbage,'center',(wRect(4)*.75),COLORS.GREEN);
-                    Screen('Flip',w);
-                    SimpExp.data(tcounter).rating = 9;
-                    %WaitSecs(.25);
-                    fprintf('%d %d %d - %d %d %d %d \n', x, y, z, buttons(1), buttons(2), buttons(3), buttons(4));
-                end
+        end
+    end        
+end
 
-            end
-        end        
-    end
-end %STARTHEREANDDELETETHISTOMORROW
+
+
+savepath = string('"%s"/"%s"/Results', path, arg);
+
+% cd(savedir)
+savename = ['SimpExp_Food_' num2str(ID)];
+
+if exist(savename,'file')==2;
+    savename = ['SimpExp_Food_' num2str(Subject.id) '_' sprintf('%s_%2.0f%02.0f',Config.date,d(4),d(5))];
+end
+
+mat_savename = [savename '.mat'];
+xls_savename = [savename '.xls'];
+
+save([savepath mat_savename],'Trial');
+print('saved mat file');
+
+fields = transpose(fieldnames(Trial.data));
+out_data = transpose(struct2cell(transpose(Trial.data)));
+xlswrite([savedir xls_savename], out_data);
+print('saved xls file');
+
+
+DrawFormattedText(w,'That concludes this task.','center','center',COLORS.WHITE);
+Screen('Flip', w);
+KbWait();
+
+sca
+
+end
+
 
 
 
